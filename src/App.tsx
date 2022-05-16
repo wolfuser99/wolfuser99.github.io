83df3b99666a6384b50ef8a5ed2261f6f460451d
Juan Cid <juan.cid@cencosudscotiabank.cl>
2022-05-16 00:11:45 -0400

    feat(template): :sparkles: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@mui/material/Button";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained">Hello World</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
